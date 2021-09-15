34f432323085cb0feef23cda1859740a1fce566a
yoondabin <yundabin0608@gmail.com>
2021-09-15 17:49:50 +0900

    swift Week 01

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+var year:Int = 1999
+
+var city:String = "Seoul";
+
+
+// 한줄 주석
+
+/*
+ 여러줄 주석의 경우
+ 다음을 사용하면 좋음
+ */
+
+var account = 150;
+
+// 용돈
+account = account + 10;
+// bought apple watch
+account = account - 40;
+
+// code -> top to bottom
+
+// 프로그램 출력 -> 콘솔에 출력 print 사용
+print(45) ;
+print(account);
+print("Funny swift")
