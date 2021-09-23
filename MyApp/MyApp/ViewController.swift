db20cd373732abce8ac8f2bf7c6a22e34a473f86
yoondabin <yundabin0608@gmail.com>
2021-09-23 14:39:14 +0900

    Section 3 - Create UI

--- MyApp/MyApp/ViewController.swift
+++ MyApp/MyApp/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  MyApp
-//
-//  Created by yundabin on 2021/09/14.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var countDown = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onButtonClick(_ sender: Any) {
+        countDown = countDown - 5;
+        welcomeLabel.text = String(countDown);
+        
+        if countDown <= 50 {
+            welcomeLabel.textColor = .red;
+        } else if  countDown <= 10 {
+            welcomeLabel.textColor = .blue;
+        }
+    }
+    
+    
 }
 
